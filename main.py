8a22139485620696cb530e3b9acdb4b57f22f561
rk2-182 <ricardoplus@live.cl>
2021-09-24 15:09:06 -0300

    2.se corrige el codigo para ejecutar desde cmd Windows con urta ansoluta, tambien se comprueba existencia de archivo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 """
 Un código QR (del inglés Quick Response code, «código de respuesta rápida») es la evolución del código de barras.
 Es un módulo para almacenar información en una matriz de puntos o en un código de barras bidimensional.
+
+apoyo: https://www.youtube.com/watch?v=FD6bXAudZT0&t
+https://binary-coffee.dev/post/generando-codigos-qr-con-python
+
 """
 import qrcode
 import os
@@ -15,20 +19,21 @@ imagen = qrcode.make(texto)
 
 NombreImg = input("Introduzca el nombre de la imagen QR: ") + '.png'
 
-ruta=str(pathlib.Path().absolute())+'/GenerarQR/qr/'+NombreImg
+ruta=str(pathlib.Path().absolute())+'\\qr\\'+NombreImg
 #Abrimos un archivo en modo escritura que es donde se guardará nuestro código.
 f = open(ruta,'wb')
 
 
 if os.path.exists(ruta):
-    print("Existe")
+    print("Existe: ",ruta)
 else:
     print("No existe")
 
 imagen.save(f)
 f.close()
 
+
 #Abrir imagen
-ruta_imagen = "GenerarQR/qr/"+NombreImg
+ruta_imagen = ruta
 Image.open(ruta_imagen).show()
 
